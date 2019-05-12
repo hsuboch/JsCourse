80658adb54c9f4d4661bb1e0ddc663480f4a8da9
e2.suboch <korbrakit@gmail.com>
2019-05-13 00:54:27 +0300

    implemented 4-6

--- pages/resultsPage/index.js
+++ pages/resultsPage/index.js
@@ -1,5 +1,5 @@
 const {locators} = require('./constants');
-const BasePage = require('../../framework/basePage')
+const BasePage = require('../../framework/basePage');
 
 class ResultsPage extends BasePage {
     constructor(browser) {
@@ -7,17 +7,17 @@ class ResultsPage extends BasePage {
     }
 
     async getCountOfResults() {
-        var results = await this.browser.findElement(locators.resultsCount, 'Search input');
-        var countOfResults = await results.getText();
+        const results = await this.browser.findElement(locators.resultsCount, 'Search input');
+        const countOfResults = await results.getText();
         return countOfResults.split(' ').join('').match('\\d+')[0];
     }
 
     async getResultsLinks() {
-        var results = [];
-        var elements = await this.browser.findElements(locators.links, 'Search input');
-        elements.forEach(function (element){
-            results.push(element.getText());
-        });
+        let results = [];
+        const elements = await this.browser.findElements(locators.links, 'Search input');
+        for (let i = 0; i < elements.length; i++) {
+            results.push(await elements[i].getText());
+        }
         return results;
     }
 }
