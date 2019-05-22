5613094db72a9fbd0e48b5d65291f01d2015c9b3
e2.suboch <korbrakit@gmail.com>
2019-05-22 20:21:00 +0300

    workshop 7

---
+++ protractor/demo.spec.js
@@ -0,0 +1,31 @@
+const {eventFire} = require('./js/sendEvent');
+describe('Protractor Demo App', () => {
+    const username = element(by.id('login'));
+    const password = element(by.id('password'));
+    const logIn = element(by.css('.btn-primary'));
+    const logOut = element(by.id('logout'));
+    const pageHeader = element(by.css('.panel-heading h2'));
+
+    beforeEach(() => {
+        browser.get('http://reportingportal:8080');
+    });
+
+    it('should have title', () => {
+        expect(browser.getTitle()).toEqual('Reporting Portal');
+    });
+
+    it('Should log in with admin', () => {
+        username.sendKeys('admin');
+        password.sendKeys('123456');
+        //logIn.click();
+        browser.executeScript(eventFire, logIn, 'submit');
+        expect(pageHeader.getText()).toEqual('Select Project');
+
+    });
+
+    it('Should log out', () => {
+        logOut.click();
+
+        expect(username.isPresent()).toBe(true);
+    });
+});
